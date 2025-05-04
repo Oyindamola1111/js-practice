718b8da0e0d096218671423702df1de0cde07366
Oyindamola1111 <ajalajohnson27@gmail.com>
2025-05-05 00:16:10 +0100

    my practice note

---
+++ script.js
@@ -0,0 +1,133 @@
+// let fruits=["mango","cherry","apple","lime","cake"];
+// console.log(fruits);
+
+// let man=["mango","cherry","apple","lime","cake"];
+// console.log(man[4]);
+
+// let woman=["pin","biro","gun","key","bike","shaft"];
+// woman.splice(2,0,"mob");
+// console.log(woman);
+
+// let seun=["cake","gun","house","junk","love"];
+// seun.splice(4,1,)
+// console.log(seun);
+
+// let kamo=["open",["grape","queen"],"loko","grass","rom"];
+// kamo.splice(0,1,"mango");
+// console.log(kamo);
+
+// let orange=[30,40,3,6,45,23,200,12,2,90,];
+// console.log(orange.length);
+
+// let kama=[90,78,3,4,2,1,5,6,55,7];
+// kama.push("bolu","tun");
+// console.log(kama);
+
+// //if in array
+// let age=18;
+// if(age>=18) {console.log("you are eligible");} 
+
+// //if...else
+// let you=19;
+// if (you>=19) {console.log("yes");}
+// else{console.log("no");}
+
+// //else .... if handles multiple conditions
+
+// let score=75;
+// if(score>=90) {console.log("A grade");}
+// else if(score>=80) {console.log("B grade");}
+// else if(score>=70) {console.log("C grade");}
+// else{console.log("Failed");}
+
+// //array.include
+
+// let message=["hello","world","you","group","big"];
+// console.log(message.includes("world"));
+// console.log(message.includes("you"));
+// console.log(message.includes("pain"));
+
+
+
+
+
+
+// Function to increment number and change color
+// function increase() {
+//     let numberBox = document.getElementById("number");
+//     let currentNumber = parseInt(numberBox.innerText);
+//     numberBox.innerText = currentNumber + 1;
+
+//     // Change color to red (you can change this to any color or random color later)
+//     numberBox.style.color = "red";
+//   }
+
+  
+
+
+//   let numberBox = document.getElementById("number");
+
+//   // Define the function to increase the number
+//   function increase() {
+//     let currentNumber = parseInt(numberBox.innerText);
+//     numberBox.innerText = currentNumber + 1;
+//     numberBox.style.color = "red";  // Change color to red
+//   }
+
+
+
+
+//   let teewhy = document.getElementById("number")
+//   function dami() {
+//     console.log("teewhy ");
+    
+//   }
+
+
+
+//   let stone = document.getElementById("number")
+
+
+//   function change() {
+//     stone.innerText = "my name is teewhy"
+//   }
+
+// let bag = document.getElementById("number")
+
+// function change() {
+//     bag.innerText = "Abdul Maleek"
+// }
+
+// let score = document.getElementById("age");
+
+//     function increase() {
+//       let number = parseInt(score.innerText);
+//       score.innerText = number + 1;
+//     }
+
+//     function decrease() {
+//       let number = parseInt(score.innerText);
+//       if (number > 0) {
+//         score.innerText = number - 1;
+//       } else {
+//        alert("Can't go below 0")
+//       }
+//     }
+
+
+
+let a = document.getElementById("number");
+function change() {
+  let b = parseInt(a.innerText);
+  a.innerText = b + 1;
+a.style.color="red";
+a.style.backgroundColor="yellow";
+
+}
+
+
+
+//   if (b <= 10){
+//     a.innerText = b + 1 ;
+//   } else {alert("cant go further")}
+// }
