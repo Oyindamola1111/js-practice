f971fb24843befd5e1a2c423b69d320712faac89
Oyindamola1111 <ajalajohnson27@gmail.com>
2025-05-15 12:19:15 +0100

    added new things

--- script.js
+++ script.js
@@ -116,14 +116,12 @@
 
 
 
-let a = document.getElementById("number");
-function change() {
-  let b = parseInt(a.innerText);
-  a.innerText = b + 1;
-a.style.color="red";
-a.style.backgroundColor="yellow";
-
-}
+// let a = document.getElementById("number");
+// function change() {
+//   let b = parseInt(a.innerText);
+//   a.innerText = b + 1;
+// a.style.color="blue";
+// a.style.backgroundColor="yellow";
 
 
 
@@ -131,3 +129,123 @@ a.style.backgroundColor="yellow";
 //     a.innerText = b + 1 ;
 //   } else {alert("cant go further")}
 // }
+
+
+// let start = document.getElementById("digit");
+// function increase() {
+//   let number = parseInt(start.innerText);
+//   start.innerText = number + 1;
+//   start.style.color="green";
+//   start.style.fontSize="40px";
+// }
+// // if(number <= 11){
+// //   start.innerText = number + 1;
+// // } else{alert("cant go further")}
+
+// function decrease() {
+//   let number = parseInt(start.innerText);
+//   start.innerText = number - 1;
+//   if (number <= 1){
+//     alert("cant go below 0")
+//   }
+//   start.style.color="red";
+//   start.style.fontSize="40px";
+// }
+
+// let book = document.getElementById("mali");
+// function increase() {
+//   let note = parseInt(book.innerText);
+//   book.innerText = note + 1;
+//   book.style.color="green";
+//   book.style.fontSize="40px";
+  
+// }
+
+// function decrease() {
+//   let note = parseInt(book.innerText);
+//   book.innerText = note - 1;
+//   book.style.color="red";
+//   book.style.fontSize="40px";
+  
+// }
+
+
+
+// string methods .... a must know
+
+// let myStr = "Hello javascript World";
+// console.log(myStr);
+// console.log(myStr.toLowerCase());
+// console.log(myStr.toUpperCase());
+// console.log(myStr.charAt());
+// console.log(myStr.indexOf("World"));
+// console.log(myStr.includes("Hello"));
+// console.log(myStr.slice(2,5));
+// console.log(myStr.trim());
+// console.log(myStr.replace("World", "idiot"));
+// console.log(myStr.split());
+// console.log(myStr.split(" ")[2]);
+// console.log(myStr.startsWith("Hello"));
+// console.log(myStr.endsWith("hi"));
+
+// console.log(myStr.trim()); // takes off un-neccessary spaces in sentences
+// console.log(myStr.replace("World", "Universe")); // replace world and input universe
+
+
+// NUMBER METHODS
+
+// let num = 42.769;
+// console.log(num);
+// console.log(num.toString());
+// console.log(num.toFixed());
+
+
+
+
+// Math Method
+
+// console.log(Math.PI);
+// console.log(Math.round(41.945));
+// console.log(Math.floor(42.342));
+// console.log(Math.ceil(42.2));
+// console.log(Math.max(12,4,3,7));
+// console.log(Math.min(12,4,6,8));
+// console.log(Math.pow(23,70));
+// console.log(Math.sqrt(144));
+// console.log(Math.random()*100);
+// console.log(Math.floor(Math.random()*100));
+
+
+
+
+
+// OBJECTS
+
+// let student = {
+//     name: "shade",
+//     age: 34,
+//     mail: "shade@gmail.com"
+// };
+//  console.log(student);
+//  console.log(student.name);
+//  console.log(Object.keys(student));
+//  console.log(Object.values(student));
+
+// FUNCTIONS
+
+// function myName(name) {
+//     console.log(name);  
+// }
+// myName("damola");
+// myName("idiot");
+// myName("money");
+
+// function addNum(a, b) {
+//     console.log(a+b);
+    
+// }
+// addNum(30,8);
+
+
+
+ 
